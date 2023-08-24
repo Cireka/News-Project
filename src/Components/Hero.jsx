0761b4bdff9b37d89c2be0349ec0f2c1fc6258a5
Cireka <tsifekide@gmail.com>
2023-08-24 22:42:41 +0400

    changed api config

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,15 +4,17 @@ import moment from "moment/moment";
 const Hero = () => {
   const newsKey = import.meta.env.VITE_NEWS_KEY;
   const { data } = useFetch(
-    `https://newsdata.io/api/1/news?apikey=${newsKey}&image=1&language=en&full_content=1&size=10`
+    `https://newsdata.io/api/1/news?apikey=${newsKey}&image=1&language=en&full_content=1&size=10&excludedomain=phys.org,https://whatnowatlanta.com`
   );
-
+  // Phys.org
   const title = data?.results[0].title;
   const author = data?.results[0].source_id;
   const image = data?.results[0].image_url;
   const publishedDate = data?.results[0].pubDate;
   const content = data?.results[0]?.content.split(" ");
-  const description = content?.slice(0, 80).join(" ");
+
+  const description = content?.slice(1, 75).join(" ");
+  const firstWord = content?.[0];
 
   const hoursDifference = moment().diff(moment(publishedDate), "hours");
   return (
@@ -40,7 +42,7 @@ const Hero = () => {
         <div className="grow hidden xl:block">
           <p className="text-[18px] text-[400] leading-[188%] text-[#444]">
             <span className="text-[#444] font-[600] text-[36px] leading-[180%]">
-              dolor
+              {firstWord}
             </span>{" "}
             {description}{" "}
             <a
